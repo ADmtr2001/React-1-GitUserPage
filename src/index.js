541fe4f92c8b1059cee3e90e539bc93e0c8bef87
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-21 01:50:44 +0200

    add authorization

--- src/index.js
+++ src/index.js
@@ -6,15 +6,13 @@ import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-// dev-2847dwro.us.auth0.com
-// henvWn68VMeK3Snonf5GOlZTEFzA53Wv
-
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
       domain='dev-2847dwro.us.auth0.com'
       clientId='henvWn68VMeK3Snonf5GOlZTEFzA53Wv'
       redirectUri={window.location.origin}
+      cacheLocation='localstorage'
     >
       <GithubProvider>
         <App />
