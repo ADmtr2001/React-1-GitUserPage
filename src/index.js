14b3b46eb45738e5317d15b66be1b7ee3bbe3d61
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-20 23:49:57 +0200

    set up Auth0

--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@ import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+// dev-2847dwro.us.auth0.com
+// henvWn68VMeK3Snonf5GOlZTEFzA53Wv
+
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-2847dwro.us.auth0.com'
+      clientId='henvWn68VMeK3Snonf5GOlZTEFzA53Wv'
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
