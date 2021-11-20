2f84770f822a7ccafe01c9f3b938e340b444e79e
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-20 20:55:07 +0200

    add Search rendering

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
 const Search = () => {
+  const [user, setUser] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user) {
+    }
+  };
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <MdSearch />
+            <input
+              type='text'
+              placeholder='enter github user'
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <button type='submit'>search</button>
+          </div>
+        </form>
+        <h3>requests: 60 / 60</h3>
+      </Wrapper>
+    </section>
+  );
+
   return <h2>search component</h2>;
 };
 
